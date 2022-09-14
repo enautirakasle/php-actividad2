9c89c49809bd1d97a9037d749cf1653ca69a6750
2daw3 <2daw3@HZ114490>
2022-09-14 09:25:18 +0200

    primer commit

---
+++ ejercicio01.php
@@ -0,0 +1,8 @@
+/*
+Ejercicio 01: Hacer un programa que contenga dos 
+números distintos en sendas variables y los sume. 
+A continuación mostrar el resultado de la suma,
+la resta la multiplicación, divisioón y módulo.
+Guarde como ejercicio01.php.
+*/
+<?php
